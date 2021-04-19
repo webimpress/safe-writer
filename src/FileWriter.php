45cbee0e0f94883ccd3459b29a8da207e5b246b8
Michał Bundyra <contact@webimpress.com>
2021-04-19 11:11:12 +0100

    Suppress warnings using custom empty error handler instead of `@`
    Fixes #49
    
    When we have a custom error handler around writeFile then the warning
    can be handled in case directory os not writable, because warnings were
    suppressed by `@` not a custom (empty) error handler.

--- src/FileWriter.php
+++ src/FileWriter.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Webimpress\SafeWriter;
 
+use function assert;
 use function chmod;
 use function dirname;
 use function file_put_contents;
+use function is_string;
 use function is_writable;
 use function realpath;
 use function rename;
+use function restore_error_handler;
+use function set_error_handler;
 use function stripos;
 use function tempnam;
 use function umask;
@@ -25,12 +29,17 @@ final class FileWriter
     public static function writeFile(string $file, string $content, int $chmod = 0666) : void
     {
         $dir = dirname($file);
+
         // suppress notice thrown when falling back to system temp dir
-        $tmp = @tempnam($dir, 'wsw');
+        $tmp = self::suppress(static function () use ($dir) {
+            return tempnam($dir, 'wsw');
+        });
 
         if ($tmp === false) {
             throw Exception\RuntimeException::unableToCreateTemporaryFile($dir);
         }
+
+        assert(is_string($tmp));
 
         if (dirname($tmp) !== realpath($dir)) {
             unlink($tmp);
@@ -48,13 +57,31 @@ final class FileWriter
         }
 
         // On windows try again if rename was not successful but target file is writable.
-        while (@rename($tmp, $file) === false) {
+        while (self::suppress(static function () use ($tmp, $file) {
+            return rename($tmp, $file);
+        }) === false) {
             if (is_writable($file) && stripos(PHP_OS, 'WIN') === 0) {
                 continue;
             }
 
             unlink($tmp);
             throw Exception\RenameException::unableToMoveFile($tmp, $file);
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    private static function suppress(callable $func)
+    {
+        /** @psalm-suppress InvalidArgument */
+        set_error_handler(static function () : void {
+        });
+
+        try {
+            return $func();
+        } finally {
+            restore_error_handler();
         }
     }
 }
