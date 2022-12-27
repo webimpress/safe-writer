a4953daa5b819ea624cd35e6548f695d4f45147d
Michał Bundyra <contact@webimpress.com>
2022-12-27 14:43:39 +0100

    update tests

--- test/safe-writer.php
+++ test/safe-writer.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 include __DIR__ . '/../vendor/autoload.php';
 
-$data = array_fill(0, random_int(10, 100), $_SERVER);
+$data = array_fill(0, 100, $_SERVER);
 
-// sleep for something between 0.5-2s
-usleep(random_int(500000, 2000000));
+// sleep for something between 0.1-2s
+usleep(random_int(100000, 2000000));
 
 Webimpress\SafeWriter\FileWriter::writeFile(
     __DIR__ . '/test.php',
